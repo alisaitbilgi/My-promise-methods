89f4dc3d7bbee7cc0e8401cc9fc3504f3fa8404c
ali sait bilgi <alisaitbilgi@gmail.com>
2016-12-20 11:24:31 +0200

    Update Method.test.js

--- Method.test.js
+++ Method.test.js
@@ -2,83 +2,85 @@
 
 
 const MyPromise = require("./myMethod");
-let testArray = [],
-  promiseArrayFalsy = [],
-  promiseArray = [],
-  rejectedBefore = [],
-  resolvedBefore = [];
 
-for(let i = 0; i < 10; i++) {
-  testArray[i] = i;
-  promiseArray[i] = new Promise(res => {
-    res(i);
-  });
-  promiseArrayFalsy[i] = new Promise((res, rej) => {
-    rej(i);
-  });
-  rejectedBefore[i] = new Promise((res, rej) => {
-    if(i === 0) {
-      rej(i);
+let testArrayForValueCheck = [];
+let promiseArrayFalsy = [];
+let promiseArrayTruthy = [];
+let rejectedBefore = [];
+let resolvedBefore = [];
+
+describe("", () => {
+  
+  // test values are created here.  
+  beforeAll(() => {
+    for(let i = 0; i < 10; i++) {
+      testArrayForValueCheck[i] = i;
+
+      promiseArrayTruthy[i] = Promise.resolve(i);
+
+      promiseArrayFalsy[i] = Promise.reject(i);
+
+      if(i === 0) 
+        rejectedBefore[i] = Promise.reject(i);
+      else
+        rejectedBefore[i] = Promise.resolve(i);
+      
+      if(i % 2 === 0) 
+        resolvedBefore[i] = Promise.resolve(i);
+      else
+        resolvedBefore[i] = Promise.reject(i);
     }
-    else {
-      res(i);
-    }
-  });
-  resolvedBefore[i] = new Promise((res, rej) => {
-    if(i % 2 === 0) {
-      res(i);
-    }
-    else {
-      rej(i);
-    }
-  });
-}
-
-describe("Testing MyPromise.all method..", () => {
-
-  it("When all of the promises in the array are resolved state", () => {
-    return MyPromise.all(promiseArray)
-      .then(result => expect(result).toEqual(testArray));
   });
 
-  it("When all of the promises in the array are rejected state", () => {
-    return MyPromise.all(promiseArrayFalsy)
-      .catch(result => expect(result).toEqual(testArray[0]));
+  describe("MyPromise.all method should return", () => {
+
+    it("all of the promises in the array, which are resolved state", () => {
+      return MyPromise.all(promiseArrayTruthy)
+        .then(result => expect(result).toEqual(testArrayForValueCheck));
+    });
+
+    it("first promise in the array, which is rejected state", () => {
+      return MyPromise.all(promiseArrayFalsy)
+        .catch(result => expect(result).toEqual(testArrayForValueCheck[0]));
+    });
+
+    it("first rejected promise comes before the resolved ones", () => {
+      return MyPromise.all(rejectedBefore)
+        .catch(result => expect(result).toEqual(testArrayForValueCheck[0]));
+    });
+
+    it("first rejected promise comes after the resolved ones", () => {
+      return MyPromise.all(resolvedBefore)
+        .catch(result => expect(result).toEqual(testArrayForValueCheck[1]));
+    });
+
   });
 
-  it("When the rejected promise comes before the resolved ones", () => {
-    return MyPromise.all(rejectedBefore)
-      .catch(result => expect(result).toEqual(testArray[0]));
+
+  describe("MyPromise.race method should return", () => {
+
+    it("first promise in the array which is resolved state", () => {
+      return MyPromise.race(promiseArrayTruthy)
+        .then(result => expect(result).toEqual(testArrayForValueCheck[0]));
+    });
+
+    it("first promise in the array which is rejected state", () => {
+      return MyPromise.race(promiseArrayFalsy)
+        .catch(result => expect(result).toEqual(testArrayForValueCheck[0]));
+    });
+
+    it("first rejected promise comes before the resolved ones", () => {
+      return MyPromise.race(rejectedBefore)
+        .catch(result => expect(result).toEqual(testArrayForValueCheck[0]));
+    });
+
+    it("first resolved promise comes before the rejected ones", () => {
+      return MyPromise.race(resolvedBefore)
+        .then(result => expect(result).toEqual(testArrayForValueCheck[0]));
+    });
+
   });
 
-  it("When the resolved promise comes before the rejected ones", () => {
-    return MyPromise.all(resolvedBefore)
-      .catch(result => expect(result).toEqual(testArray[1]));
-  });
 
-});
+})
 
-
-describe("Testing MyPromise.race method..", () => {
-
-  it("When all of the promises in the array are resolved state", () => {
-    return MyPromise.race(promiseArray)
-      .then(result => expect(result).toEqual(testArray[0]));
-  });
-
-  it("When all of the promises in the array are rejected state", () => {
-    return MyPromise.race(promiseArrayFalsy)
-      .catch(result => expect(result).toEqual(testArray[0]));
-  });
-
-  it("When the rejected promise comes before the resolved ones", () => {
-    return MyPromise.race(rejectedBefore)
-      .catch(result => expect(result).toEqual(testArray[0]));
-  });
-
-  it("When the resolved promise comes before the rejected ones", () => {
-    return MyPromise.race(resolvedBefore)
-      .then(result => expect(result).toEqual(testArray[0]));
-  });
-
-});
